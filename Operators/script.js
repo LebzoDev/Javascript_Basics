414e48aedb3813ee1ee8638df36f4ea7d053fc48
lebzodev <leboundiayee123@gmail.com>
2021-09-30 00:52:46 +0000

    operators pluralsight

---
+++ Operators/script.js
@@ -0,0 +1,17 @@
+// JAVASCRIPT EQUALITY OPERATORS ( == && === && Object.is() )
+
+//Not type safe  (==)
+//Type safe     (=== && Object.is())
+
+console.log("34"==34); //true
+
+console.log(0==false);//true
+
+console.log(null==undefined); //true
+
+console.log(""==0); //true
+
+console.log([1,3]=="1,3");//true
+
+
+console.log(1/"a")
